44e857133e5fdd0c58fb68035b2aabf66f28ee87
Tomas Perez <63325595+tomasperez13@users.noreply.github.com>
2021-02-16 12:17:27 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,149 @@
+"""
+CSC 412
+ICA #3
+2/5/2021
+
+@author: Tomas Perez
+"""
+from queue import Queue
+
+totalNodesVisited = 0
+fastestRoute = []
+start_position = [0, 1]
+end_position = [11, 11]
+rows, cols = (11, 11)
+visited = [[False] * cols] * rows
+row_queue = Queue
+column_queue = Queue
+
+
+def print_maze(user_maze):
+    for row in user_maze:
+        for c in row:
+            print(c, end='')
+        print('')
+
+
+def bfs():
+    row_queue.put(start_position[0])
+    column_queue.put(start_position[1])
+    solution = Queue()
+    solution.put("")
+    found_the_end = False
+    # cell = maze[row][column]
+    visited[start_position[0]][start_position[1]] = True
+    while solution.qsize() > 0:
+        temp_path = solution.get()
+        row = row_queue.get()
+        column = column_queue.get()
+        current_position = (row, column)
+        #FIXME: Debating on whether or not to use explore neighbours...
+        for j in ["L", "R", "U", "D"]:
+            put = temp_path + j
+            if reachable(current_position, put):
+                if visited[current_position[0]][current_position[1]] is not True:
+                    solution.put(put)
+    return solution
+
+
+def update_current_pos(direction):
+    if direction ==
+
+
+# def find_end(coordinates, moves):
+#     start_row = coordinates[0]
+#     start_column = coordinates[1]
+#     for move in moves:
+#         if move == "L":
+#             i -= 1
+#         elif move == "R":
+#             i += 1
+#         elif move == "U":
+#             j -= 1
+#         elif move == "D":
+#             j += 1
+#     if i == 11 and j == 11:
+#         print("Found: " + moves)
+#         # print_Solution(maze, moves)
+#         return True
+#     return False
+
+
+# def print_Solution(maze, moves):
+#     i = 0;
+#     j = 0;
+#     pos = set()
+#     for move in path:
+#         if move ==
+
+
+""" This is ICA 6 """
+
+"""
+This method receives a coordinate for a maze and a direction
+and determines whether the desired cell is reachable or not.
+@coordinate:param - location on the maze in the form of [row, column]
+@direction:param - refers to a lettered representation of direction, 
+                    such as 'U', 'D', 'L', 'R'
+@:returns - true if cell is reachable, false if not. 
+"""
+
+
+def reachable(coordinate, direction):
+    row = coordinate[0]
+    column = coordinate[1]
+    cell = maze[row][column]
+    adjacent_cell = ""
+    if direction == 'U':
+        if '¯' in cell:
+            return False
+    # Special case where we need to look at an adjacent cell.
+    elif direction == 'D':
+        adjacent_cell = maze[row + 1][column]
+        if '¯' in adjacent_cell:
+            return False
+    # Special case where we need to look at an adjacent cell.
+    elif direction == 'L':
+        adjacent_cell = maze[row][column - 1]
+        if '|' in adjacent_cell:
+            return False
+    elif direction == 'R':
+        if '|' in cell:
+            return False
+    return True
+
+
+maze = [[" |", "¯|", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯|"],
+        [" |", " |", " |", "¯ ", "¯|", "  ", "  ", "  ", "  ", "  ", " |"],
+        [" |", " |", " |", " |", "¯ ", "  ", "  ", "  ", "  ", "  ", " |"],
+        [" |", "  ", "¯ ", "  ", "¯ ", "¯ ", "¯|", "  ", "  ", "  ", " |"],
+        [" |", "¯ ", "¯ ", "¯ ", "¯ ", "¯|", "  ", "¯ ", "¯ ", "¯|", " |"],
+        [" |", "  ", "  ", "  ", "  ", " |", " |", "¯ ", "¯ ", "¯ ", " |"],
+        [" |", "  ", "  ", "  ", "  ", " |", "  ", "¯ ", "¯ ", "¯ ", "¯|"],
+        [" |", "  ", "  ", "  ", "  ", " |", " |", "¯ ", "¯ ", "¯|", " |"],
+        [" |", "  ", "  ", "  ", "  ", " |", " |", "  ", "  ", " |", " |"],
+        [" |", "  ", "  ", "  ", "  ", " |", " |", "  ", "  ", " |", " |"],
+        ["¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ "]]
+
+print_maze(maze)
+bfs()
+
+print()
+print()
+
+""" Dr. Kolta's Maze """
+# 11 x 11 version
+maze = [[" |", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯|"],
+        [" |", " |", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", " |"],
+        [" |", " |", "  ", "¯ ", "¯ ", "¯ ", "¯|", "¯ ", "¯ ", "¯ ", " |"],
+        [" |", " |", "¯ ", "¯ ", "¯ ", " |", "¯ ", "¯ ", "¯ ", "¯ ", "¯|"],
+        [" |", " |", "  ", "  ", "  ", " |", "  ", "¯|", "¯ ", "¯|", " |"],
+        [" |", " |", "  ", "  ", "  ", "  ", "¯ ", "¯ ", "¯ ", "¯ ", " |"],
+        [" |", " |", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", " |"],
+        [" |", " |", " |", "¯ ", "¯|", "¯ ", "¯|", "¯|", "¯|", "¯ ", " |"],
+        [" |", " |", " |", " |", " |", " |", " |", "  ", " |", "  ", "¯|"],
+        [" |", " |", "  ", " |", "  ", " |", "  ", "¯ ", "  ", "¯ ", " |"],
+        ["¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ ", "¯ "]]
+
+print_maze(maze)
+# bfs(maze)
